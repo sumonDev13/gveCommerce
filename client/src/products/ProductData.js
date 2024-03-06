0e9a893e914c9e27e47a9cccbac77efe7c5f9b43
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-06 15:45:59 +0600

    logo setup

--- client/src/products/ProductData.js
+++ client/src/products/ProductData.js
@@ -2,37 +2,37 @@ export const productData = [
     {
       name: 'Product 1',
       description: 'This is the description for Product 1',
-      imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+      imageUrl: "public/images/gv.svg",
       price: 9.99
     },
     {
       name: 'Product 2',
       description: 'This is the description for Product 2',
-      imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+      imageUrl: "public/images/gv.svg",
       price: 14.99
     },
     {
         name: 'Product 3',
         description: 'This is the description for Product 3',
-        imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+        imageUrl: "public/images/gv.svg",
         price: 34.99
       },
       {
         name: 'Product 4',
         description: 'This is the description for Product 4',
-        imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+        imageUrl: "public/images/gv.svg",
         price: 55.99
       },
       {
         name: 'Product 5',
         description: 'This is the description for Product 5',
-        imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+        imageUrl: "public/images/gv.svg",
         price: 66.99
       },
       {
         name: 'Product 6',
         description: 'This is the description for Product 6',
-        imageUrl: "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+        imageUrl: "public/images/gv.svg",
         price: 77.99
       },
 
