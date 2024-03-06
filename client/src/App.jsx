c6c7ef10bdad6d2d20d82d241e2d524699c064a9
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-06 09:41:04 +0600

    wip-client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
     </>
   );
 }
