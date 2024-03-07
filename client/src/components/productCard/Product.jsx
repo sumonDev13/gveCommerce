e02b63f2ddd39ae980a56d1826dc2f92432f4f00
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-07 15:19:19 +0600

    quantity in card

--- client/src/components/productCard/Product.jsx
+++ client/src/components/productCard/Product.jsx
@@ -1,7 +1,7 @@
 
 
 const Product = ({ product }) => {
-  const { name, description, imageUrl, price } = product;
+  const { name, description, imageUrl, price,stockQuantity } = product;
 
   return (
     <div className="card card-compact max-w-80  shadow-xl my-4 sm:mx-4 bg-[#A7CDCC] transition duration-900 ease-in-out hover:transform hover:-translate-y-1 hover:scale-70 ... ">
@@ -9,7 +9,10 @@ const Product = ({ product }) => {
         <img src={imageUrl} alt={name} />
       </figure>
       <div className="card-body">
+        <div className="flex flex-row justify-between">
         <h2 className="card-title">{name}</h2>
+        <h4 className="card-title font-light text-sm">Stock({stockQuantity})</h4>
+        </div>
         <p>{description}</p>
         <div className="card-actions justify-between">
           <div className="inline-block rounded bg-red-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-red-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-red-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-red-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">${price}</div>
--- client/src/products/ProductData.js
+++ client/src/products/ProductData.js
@@ -3,37 +3,43 @@ export const productData = [
       name: 'Product 1',
       description: 'This is the description for Product 1',
       imageUrl: "public/images/gv.svg",
-      price: 9.99
+      price: 9.99,
+      stockQuantity:10
     },
     {
       name: 'Product 2',
       description: 'This is the description for Product 2',
       imageUrl: "public/images/gv.svg",
-      price: 14.99
+      price: 14.99,
+      stockQuantity:10,
     },
     {
         name: 'Product 3',
         description: 'This is the description for Product 3',
         imageUrl: "public/images/gv.svg",
-        price: 34.99
+        price: 34.99,
+        stockQuantity:10
       },
       {
         name: 'Product 4',
         description: 'This is the description for Product 4',
         imageUrl: "public/images/gv.svg",
-        price: 55.99
+        price: 55.99,
+        stockQuantity:10
       },
       {
         name: 'Product 5',
         description: 'This is the description for Product 5',
         imageUrl: "public/images/gv.svg",
-        price: 66.99
+        price: 66.99,
+        stockQuantity:10
       },
       {
         name: 'Product 6',
         description: 'This is the description for Product 6',
         imageUrl: "public/images/gv.svg",
-        price: 77.99
+        price: 77.99,
+        stockQuantity:10
       },
 
   ];
