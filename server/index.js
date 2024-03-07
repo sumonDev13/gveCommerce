083b9e545b3a330412bf1737ac325f69b7718734
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-07 16:19:27 +0600

    server db connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('backend')
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(port,()=>{
+    console.log(`server listening on ${port}`);
+})
