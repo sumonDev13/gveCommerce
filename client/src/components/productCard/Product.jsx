b47ae444982a3216c6100167a4e56019720d656e
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-07 09:19:32 +0600

    client styling(card buttons)

--- client/src/components/productCard/Product.jsx
+++ client/src/components/productCard/Product.jsx
@@ -11,8 +11,9 @@ const Product = ({ product }) => {
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
-        <div className="card-actions justify-center">
-          <button className="btn btn-primary ">Buy Now (${price})</button>
+        <div className="card-actions justify-between">
+          <div className="inline-block rounded bg-red-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-red-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-red-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-red-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">${price}</div>
+          <button className="inline-block rounded bg-cyan-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-cyan-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-cyan-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-cyan-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0">Buy Now</button>
         </div>
       </div>
     </div>
